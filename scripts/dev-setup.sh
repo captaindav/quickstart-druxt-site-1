f536a4c23ec253686c4ac0406dc7d3b34efccfe5
Dave Connerth <captaindav@gmail.com>
2022-04-05 20:11:35 -0400

    Added script for setting up development environment.

---
+++ scripts/dev-setup.sh
@@ -0,0 +1,30 @@
+cd druxtjs/packages/breadcrumb
+npm link
+cd ../docgen
+npm link
+cd ../druxt
+npm link
+cd ../entity
+npm link
+cd ../menu
+npm link
+cd ../router
+npm link
+cd ../schema
+npm link
+cd ../site
+npm link
+cd ../test-utils
+npm link
+cd ../views
+npm link
+
+cd ../../nuxt
+npm link breadcrumb
+npm link druxt
+npm link entity
+npm link menu
+npm link site
+npm link text-utils
+npm link views
+
